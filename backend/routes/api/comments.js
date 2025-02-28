ac52a345b1203a7c9e1e07569001130ee1e6d2a6
WilcoApp <hello@wilco.gg>
2025-02-28 05:09:47 +0000

    Initial commit

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -1,28 +1,5 @@
-
-
 const router = require("express").Router();
 const mongoose = require("mongoose");
 const Comment = mongoose.model("Comment");
 
 module.exports = router;
-
-router.get("/", async (req, res) => {
-    try {
-        const comments = await Comment.find();
-        res.json({ comments });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
-
-// Add another endpoint for deleting a comment
-router.delete("/:id", async (req, res) => {
-    try {
-        await Comment.findByIdAndRemove(req.params.id);
-        res.json({ success: true });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
